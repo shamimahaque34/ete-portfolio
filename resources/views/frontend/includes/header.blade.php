05c6c79d4ff540673d86b8ebe09525d917472d4a
Shamima Haque <haqueshamima34@gmail.com>
2024-07-16 12:46:21 +0600

    necessary controller and model migration files added

--- resources/views/frontend/includes/header.blade.php
+++ resources/views/frontend/includes/header.blade.php
@@ -17,7 +17,7 @@
         <a class="nav-link" href="{{ route('home') }}">Home</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="{{ route('about') }}">About</a>
+        <a class="nav-link" href="{{ route('about') }}" >About</a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="{{ route('services') }}">Services</a>
